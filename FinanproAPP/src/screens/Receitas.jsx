3dde98a6f98cec0a31a392c630ad407f70c62030
LucasBarrosds <134096109+LucasBarrosds@users.noreply.github.com>
2024-09-12 14:58:24 -0300

    Telas
    Criação das Telas de Backup, Despesas, Dicas e Receitas

--- FinanproAPP/src/screens/Receitas.jsx
+++ FinanproAPP/src/screens/Receitas.jsx
@@ -2,10 +2,10 @@ import { View, Text, Button } from "react-native";
 import styles from "../styles/styles";
 import CustomButton from "../components/CustomButton";
 
-export default function AddReceitas({navigation}) {
+export default function Receitas({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>AddReceitas</Text>
+      <Text>Receitas</Text>
     </View>
   );
 }
